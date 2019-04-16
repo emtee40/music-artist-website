2c1e1921200be3bd54b910b7ea2579b354cb4704
Nicolai Süper <nico@k40s.net>
2019-04-16 19:56:02 +0200

    reconfigure storybook

---
+++ src/components/StartButton/StartButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./StartButton.scss";
+
+export default function StartButton() {
+    return (
+        <div className="StartButton">
+            <i className="WindowsIcon"/>
+            Start
+        </div>
+    )
+}
---
+++ src/components/StartButton/StartButton.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import {storiesOf} from "@storybook/react";
+import StartButton from "./StartButton";
+
+storiesOf("Components/Start Button",module).add("default", () => <StartButton/>);
