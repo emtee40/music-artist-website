4fcad24d54ccbc0af2a286ce5cb8faf3c4e4c7a2
Lukas Fülling <lukas@k40s.net>
2020-07-19 00:51:48 +0200

    switch to react-piwik
    It doesn't seem to work right with BrowserRouter, I might revert this later...

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import App from "./pages/app";
 import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from "react-router-dom";
+import ReactPiwik from 'react-piwik';
+import {createBrowserHistory} from "history";
 
+const customHistory = createBrowserHistory();
+const matomo = new ReactPiwik({
+    url: 'analytics.lrk.sh',
+    siteId: 5,
+    trackErrors: true,
+    jsFilename: 'matomo.js',
+    phpFilename: 'matomo.php'
+});
+
+ReactPiwik.push(['trackPageView']);
 ReactDOM.render(
-    <Router>
+    <Router history={matomo.connectToHistory(customHistory)}>{/*TODO: find a way to use this thing with BrowserRouter...*/}
         <App/>
     </Router>, document.getElementById('root'));
