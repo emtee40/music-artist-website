7121484edb981fa3181f513b235d618f53dea519
Lukas Fülling <lukas@k40s.net>
2020-07-19 03:18:27 +0200

    improve tracking

--- src/components/tracking.js
+++ src/components/tracking.js
@@ -5,9 +5,11 @@ function Tracking(props) {
     const paq = props.paq;
     const error = props.error;
 
-    if(paq) {
+    if (paq) {
+        paq.push(['deleteCustomVariables', 'page']);
+        paq.push(['setCustomUrl', '' + window.location.href]);
         paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-        if(error) {
+        if (error) {
             paq.push(['setCustomVariable', 1, 'Error', error.name, 'page']);
             paq.push(['setCustomVariable', 2, 'Message', error.message, 'page']);
             paq.push(['setCustomVariable', 2, 'Stacktrace', error.stack, 'page']);
