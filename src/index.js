90c64b62f0fdf073afdeb1302ad5744a935c4846
Lukas Fülling <lukas@k40s.net>
2020-07-19 02:07:53 +0200

    Revert "switch to react-piwik"
    This reverts commit 4fcad24d54ccbc0af2a286ce5cb8faf3c4e4c7a2.
    
     Conflicts:
    	src/index.js

--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@ import App from "./pages/app";
 import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from "react-router-dom";
-import ReactPiwik from 'react-piwik';
-import {createBrowserHistory} from "history";
 import './i18n';
 
-const customHistory = createBrowserHistory();
-const matomo = new ReactPiwik({
-    url: 'analytics.lrk.sh',
-    siteId: 5,
-    trackErrors: true,
-    jsFilename: 'matomo.js',
-    phpFilename: 'matomo.php'
-});
-
-ReactPiwik.push(['trackPageView']);
 ReactDOM.render(
     <Suspense fallback={'loading'}>
-        <Router history={matomo.connectToHistory(customHistory)}>{/*TODO: find a way to use this thing with BrowserRouter...*/}
+        <Router>
             <App/>
         </Router>
     </Suspense>, document.getElementById('root'));
