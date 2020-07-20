e211a78a6ba0a944e708498492d0f145fbc2ac7a
Lukas Fülling <lukas@k40s.net>
2020-07-20 16:38:55 +0200

    hide stream links when unavailable

--- src/components/release/release-links.js
+++ src/components/release/release-links.js
@@ -20,46 +20,54 @@ function ReleaseLinks(props) {
         <div className={'release-links__wrapper'}>
             <h2>{t('links')}</h2>
             <ul className="release-links">
+                {(release.links.apple !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:itunes')} href={release.links.apple}>
                         <FontAwesomeIcon alt={t('aria:itunes')} icon={faItunes}/>
                     </a>
-                </li>
+                </li>}
+                {(release.links.spotify !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:spotify')} href={release.links.spotify}>
                         <FontAwesomeIcon alt={t('aria:spotify')} icon={faSpotify}/>
                     </a>
-                </li>
+                </li>}
+                {(release.links.youtube !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:youtube')} href={release.links.youtube}>
                         <FontAwesomeIcon alt={t('aria:youtube')} icon={faYoutube}/>
                     </a>
-                </li>
+                </li>}
+                {(release.links.deezer !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:deezer')} href={release.links.deezer}>
                         <FontAwesomeIcon alt={t('aria:deezer')} icon={faDeezer}/>
                     </a>
-                </li>
+                </li>}
+                {(release.links.soundcloud !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:soundcloud')} href={release.links.soundcloud}>
                         <FontAwesomeIcon alt={t('aria:soundcloud')} icon={faSoundcloud}/>
                     </a>
-                </li>
+                </li>}
+                {(release.links.amazon !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:amazon')} href={release.links.amazon}>
                         <FontAwesomeIcon alt={t('aria:amazon')} icon={faAmazon}/>
                     </a>
-                </li>
+                </li>}
+                {(release.links.napster !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:napster')} href={release.links.napster}>
                         <FontAwesomeIcon alt={t('aria:napster')} icon={faNapster}/>
                     </a>
-                </li>
+                </li>}
+                {(release.links.yandex !== null) &&
                 <li>
                     <a target={'_blank'} aria-label={t('aria:yandex')} href={release.links.yandex}>
                         <FontAwesomeIcon alt={t('aria:yandex')} icon={faYandex}/>
                     </a>
-                </li>
+                </li>}
             </ul>
         </div>
     );
